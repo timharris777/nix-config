f91eba13fef5cd450c95b8a7cea6a4160ad70a6e
Tim Harris <timharris777@gmail.com>
2025-06-06 16:21:33 -0400

    Updating backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/Documents/backups
-# cp ~/.gitconfig ~/Documents/backups
-# cp ~/.gitconfig-cfacorp  ~/Documents/backups
+cp ~/.gitconfig ~/Documents/backups
+cp ~/.gitconfig-cfacorp  ~/Documents/backups
 cp ~/.saml2aws  ~/Documents/backups
 rsync -av ~/.aws/ ~/Documents/backups/.aws
-# rsync -av --exclude='*.lock' ~/.gnupg/ ~/Documents/backups/.gnupg
+rsync -av --exclude='*.lock' ~/.gnupg/ ~/Documents/backups/.gnupg
